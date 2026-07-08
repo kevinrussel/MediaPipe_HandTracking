8c01a9c70637cd916c9a38af4cc37dd0b4032f1d
Kevin Russel <kzone159@gmail.com>
2026-07-08 14:56:08 -0500

    adding code

--- handtracking.py
+++ handtracking.py
@@ -1,3 +1,11 @@
-import csv
+import cv2
 import mediapipe as mp
 import time
+
+cap = cv2.VideoCapture(1)
+
+while True:
+    ## getting frame
+    success, image = cap.read() 
+    cv2.imshow("Image", image)
+    cv2.waitKey(1)
