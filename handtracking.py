bd417e2babc88c8403a82d68c7694bb1088329d4
Kevin Russel <kzone159@gmail.com>
2026-07-08 15:04:54 -0500

    adding code

--- handtracking.py
+++ handtracking.py
@@ -4,8 +4,12 @@ import time
 
 cap = cv2.VideoCapture(0)
 
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(static_image_mode=False,max_num_hands=2,min_detection_confidence=0.5,min_tracking_confidence=0.5)
+
+
 while True:
     ## getting frame
     success, image = cap.read() 
     cv2.imshow("Image", image)
-    # cv2.waitKey(1)
+    cv2.waitKey(1)
