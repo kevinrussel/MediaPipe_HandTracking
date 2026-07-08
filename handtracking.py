7444b09fb0185d8c42c08ad8c6eb8e40efdabc17
Kevin Russel <kzone159@gmail.com>
2026-07-08 15:06:47 -0500

    processing image

--- handtracking.py
+++ handtracking.py
@@ -11,5 +11,6 @@ while True:
     ## getting frame
     success, image = cap.read() 
     imageRBG = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    results = hands.process(imageRBG)
     cv2.imshow("Image", image)
     cv2.waitKey(1)
