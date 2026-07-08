caaf88cbc9be63bc39b55dbc99719b9d399e9a52
Kevin Russel <kzone159@gmail.com>
2026-07-08 15:06:11 -0500

    adding code

--- handtracking.py
+++ handtracking.py
@@ -7,9 +7,9 @@ cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(static_image_mode=False,max_num_hands=2,min_detection_confidence=0.5,min_tracking_confidence=0.5)
 
-
 while True:
     ## getting frame
     success, image = cap.read() 
+    imageRBG = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
     cv2.imshow("Image", image)
     cv2.waitKey(1)
