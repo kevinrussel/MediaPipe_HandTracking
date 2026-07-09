81e1a1ff855099e86eaf9c2ad66a6b108191f7c2
Kevin Russel <kzone159@gmail.com>
2026-07-08 21:47:37 -0500

    fixing code

--- handtrackingmodule.py
+++ handtrackingmodule.py
@@ -19,36 +19,40 @@ class handDetector():
         results = self.hands.process(imageRBG)
         if results.multi_hand_landmarks:
             for hand in results.multi_hand_landmarks:
-                for id,lndmrk in enumerate(hand.landmark):
-                    h,w,c = image.shape
-                    ## normalizing now
-                    cx,cy = int(lndmrk.x*w), int(lndmrk.y*h)
-                    print(id,cx,cy)
-                mpDraw.draw_landmarks(image,hand,mpHands.HAND_CONNECTIONS)
-def main():
-    cap = cv2.VideoCapture(0)
-    pTime = 0
-    cTime = 0
-    while True:
-    ## getting frame
-    success, image = cap.read() 
-    imageRBG = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-    results = hands.process(imageRBG)
-    
-    if results.multi_hand_landmarks:
-        for hand in results.multi_hand_landmarks:
-            for id,lndmrk in enumerate(hand.landmark):
-                h,w,c = image.shape
-                ## normalizing now
-                cx,cy = int(lndmrk.x*w), int(lndmrk.y*h)
-                print(id,cx,cy)
-            mpDraw.draw_landmarks(image,hand,mpHands.HAND_CONNECTIONS)
-    cTime = time.time()
-    fps = 1/(cTime-pTime)
-    pTime = cTime
-    cv2.putText(image,str(int(fps)),(10,70),cv2.FONT_HERSHEY_COMPLEX,3,(255,8,255))
-    cv2.imshow("Image", image)
-    cv2.waitKey(1)
+                if draw:
+                    self.mpDraw.draw_landmarks(image,hand,self.mpHands.HAND_CONNECTIONS)
+                # for id,lndmrk in enumerate(hand.landmark):
+                #     h,w,c = image.shape
+                #     ## normalizing now
+                #     cx,cy = int(lndmrk.x*w), int(lndmrk.y*h)
+                #     print(id,cx,cy)
+                # mpDraw.draw_landmarks(image,hand,mpHands.HAND_CONNECTIONS)
+        return image
+    def main(self):
+        cap = cv2.VideoCapture(0)
+        pTime = 0
+        cTime = 0
+        while True:
+        ## getting frame
+            success, image = cap.read()
+            self.findHands(image,True)
+        # imageRBG = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+        # results = hands.process(imageRBG)
+        
+        # if results.multi_hand_landmarks:
+        #     for hand in results.multi_hand_landmarks:
+        #         for id,lndmrk in enumerate(hand.landmark):
+        #             h,w,c = image.shape
+        #             ## normalizing now
+        #             cx,cy = int(lndmrk.x*w), int(lndmrk.y*h)
+        #             print(id,cx,cy)
+        #         mpDraw.draw_landmarks(image,hand,mpHands.HAND_CONNECTIONS)
+        # cTime = time.time()
+        # fps = 1/(cTime-pTime)
+        # pTime = cTime
+        # cv2.putText(image,str(int(fps)),(10,70),cv2.FONT_HERSHEY_COMPLEX,3,(255,8,255))
+        # cv2.imshow("Image", image)
+            cv2.waitKey(1)
 
 
 
