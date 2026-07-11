1f3b7df2b3608d173821d853bc8e746df08fe493
Kevin Russel <kzone159@gmail.com>
2026-07-11 14:03:58 -0500

    adding code for thumb and pointer

--- projecthand.py
+++ projecthand.py
@@ -3,9 +3,11 @@ import cv2
 import mediapipe as mp
 import time
 
-
+widthcam,height = 1000,1000
 hand = hd()
 cap = cv2.VideoCapture(0)
+cap.set(3,widthcam)
+cap.set(4,height)
 pTime = 0
 cTime = 0
 
@@ -16,13 +18,13 @@ while True:
     img = hand.findHands(image,True)
     left_hand,right_hand = hand.findposition(image)
     if(len(left_hand)!= 0) and len(right_hand) != 0:
-        lefthandpoint = (left_hand[4][1],left_hand[4][2])
-        righthandpoint = (right_hand[4][1],right_hand[4][2])
-        img = hand.draw_line(img,lefthandpoint,righthandpoint)
+        thumbpoint = (left_hand[4][1],left_hand[4][2])
+        indexpoint = (left_hand[8][1],left_hand[8][2])
+        img = hand.draw_line(img,thumbpoint,indexpoint)
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
-    cv2.putText(image,str(int(fps)),(10,70),cv2.FONT_HERSHEY_COMPLEX,3,(255,8,255))
+    cv2.putText(image,str(int(fps)),(10,70),cv2.FONT_HERSHEY_COMPLEX,2,(255,8,255))
     cv2.imshow("Image", image)
     cv2.waitKey(1)
     cv2.waitKey(1)
