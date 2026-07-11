d71934d4753d334ada5342451a37c177ebca3494
Kevin Russel <kzone159@gmail.com>
2026-07-11 13:45:49 -0500

    adding code

--- handtrackingmodule.py
+++ handtrackingmodule.py
@@ -28,7 +28,11 @@ class handDetector():
 
     def draw_line(self,image,left_point,right_point):
         return cv2.line(image,left_point,right_point,(0, 255, 0), 3)
-    def findposition(self,image,handNumber = 0,draw=True):
+    
+    '''
+    This function returns the list
+    '''
+    def findposition(self,image,handNumber = 0):
         left_hand = []
         right_hand = []
         if self.results.multi_hand_landmarks:
