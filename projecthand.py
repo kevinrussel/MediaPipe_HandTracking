a4b84c7d21e7108fad396927437c4f8ea75315d2
Kevin Russel <kzone159@gmail.com>
2026-07-16 22:19:44 -0500

    adding code

--- projecthand.py
+++ projecthand.py
@@ -33,12 +33,22 @@ class Hand_Drone:
         percent = np.interp(hypot,[30,270],[0,100])
         return percent
     
+    def drone_move_down(self,percent):
+        movement_speed = np.interp(percent,[0,40],[-20,-80])
+        return movement_speed
+    
+    def drone_move_up(self,percent):
+        movement_speed = np.interp(percent,[60,100],[20,80])
+        return movement_speed
+
     def drone_movement(self,percent):
         movement = "HOLD"
+        movement_speed = 0
         if(percent > 40 and percent < 60):
             movement = "HOLD"
         elif(percent<=40):
             movement = "DOWN"
+            movement_speed = self.drone_move_donw
         else:
             movement = "UP"
         return movement
@@ -48,6 +58,7 @@ class Hand_Drone:
             ## getting frame
             success, image = self.cap.read()
             if success:
+                movement = ""
                 percent = 0
                 img = self.hand.findHands(image,True)
                 left_hand,right_hand = self.hand.findposition(image)
@@ -63,14 +74,15 @@ class Hand_Drone:
                 fps = 1/(self.cTime-self.pTime)
                 self.pTime = self.cTime
                 filling = np.interp(percent,[0,100],[400,150])
-
-
                 cv2.putText(image,f"{str(int(fps))} fps",(10,50),cv2.FONT_HERSHEY_COMPLEX,1,(255,8,255))
                 cv2.putText(image,str(int(percent)),(500,400),cv2.FONT_HERSHEY_COMPLEX,2,(255,8,255))
-                
+                print(movement)
                 cv2.rectangle(img,(50,150),(85,400),(0,255,0),3)
                 cv2.rectangle(img,(50,int(filling)),(85,400),(0,255,0),cv2.FILLED)
                 cv2.imshow("Image", image)
-                self.udp_client_socket.sendto(self.message,(self.server_address,self.port))
+                # self.udp_client_socket.sendto(self.message,(self.server_address,self.port))
                 cv2.waitKey(1)
+
+handdrone = Hand_Drone()
+handdrone.worker()
               
