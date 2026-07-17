73a0811ed3733990c5c627fa9eff5d9085f3ac24
Kevin Russel <kzone159@gmail.com>
2026-07-16 21:39:36 -0500

    adding code

--- projecthand.py
+++ projecthand.py
@@ -19,50 +19,50 @@ class Hand_Drone:
         self.server_address = "127.0.0.1"
         self.port = 8080
 
-message = b"hello world"
+# message = b"hello world"
     def calculate_hypot(self,x1,y1,x2,y2):
         return math.hypot(x2-x1,y2-y1)
 
-udp_client_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    
-while True:
-    ## getting frame
-    success, image = cap.read()
-    img = hand.findHands(image,True)
-    left_hand,right_hand = hand.findposition(image)
-    percent = 0
-    if(len(left_hand)!= 0) and len(right_hand) == 0:
-        thumbpoint = (left_hand[4][1],left_hand[4][2])
-        indexpoint = (left_hand[8][1],left_hand[8][2])
-        hypot = calculate_hypot(left_hand[4][1],left_hand[4][2],left_hand[8][1],left_hand[8][2])
-        percent = np.interp(hypot,[30,270],[0,100])
-        if(percent > 40 and percent < 60):
-            print("HOLD")
-        elif(percent<=40):
-            print("DOWN")
-        else:
-            print("UP")
-        img = hand.draw_line(img,thumbpoint,indexpoint)
-        
-    # elif(len(left_hand)!= 0) and len(right_hand) != 0:
-    #     leftpoint = (left_hand[8][1],left_hand[8][2])
-    #     rightpoint = (right_hand[8][1],right_hand[8][2])
-    #     img = hand.draw_line(img,leftpoint,rightpoint)
-    cTime = time.time()
-    fps = 1/(cTime-pTime)
-    pTime = cTime
+    def func(self):
+        udp_client_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)   
+        while True:
+            ## getting frame
+            success, image = cap.read()
+            img = hand.findHands(image,True)
+            left_hand,right_hand = hand.findposition(image)
+            percent = 0
+            if(len(left_hand)!= 0) and len(right_hand) == 0:
+                thumbpoint = (left_hand[4][1],left_hand[4][2])
+                indexpoint = (left_hand[8][1],left_hand[8][2])
+                hypot = self.calculate_hypot(left_hand[4][1],left_hand[4][2],left_hand[8][1],left_hand[8][2])
+                percent = np.interp(hypot,[30,270],[0,100])
+                if(percent > 40 and percent < 60):
+                    print("HOLD")
+                elif(percent<=40):
+                    print("DOWN")
+                else:
+                    print("UP")
+                img = hand.draw_line(img,thumbpoint,indexpoint)
+                
+            # elif(len(left_hand)!= 0) and len(right_hand) != 0:
+            #     leftpoint = (left_hand[8][1],left_hand[8][2])
+            #     rightpoint = (right_hand[8][1],right_hand[8][2])
+            #     img = hand.draw_line(img,leftpoint,rightpoint)
+            cTime = time.time()
+            fps = 1/(cTime-pTime)
+            pTime = cTime
 
 
 
-    filling = np.interp(percent,[0,100],[400,150])
+            filling = np.interp(percent,[0,100],[400,150])
 
 
-    cv2.putText(image,f"{str(int(fps))} fps",(10,50),cv2.FONT_HERSHEY_COMPLEX,1,(255,8,255))
-    cv2.putText(image,str(int(percent)),(500,400),cv2.FONT_HERSHEY_COMPLEX,2,(255,8,255))
-    
-    cv2.rectangle(img,(50,150),(85,400),(0,255,0),3)
-    cv2.rectangle(img,(50,int(filling)),(85,400),(0,255,0),cv2.FILLED)
-    cv2.imshow("Image", image)
-    udp_client_socket.sendto(message,(server_address,port))
-    cv2.waitKey(1)
-    cv2.waitKey(1)
+            cv2.putText(image,f"{str(int(fps))} fps",(10,50),cv2.FONT_HERSHEY_COMPLEX,1,(255,8,255))
+            cv2.putText(image,str(int(percent)),(500,400),cv2.FONT_HERSHEY_COMPLEX,2,(255,8,255))
+            
+            cv2.rectangle(img,(50,150),(85,400),(0,255,0),3)
+            cv2.rectangle(img,(50,int(filling)),(85,400),(0,255,0),cv2.FILLED)
+            cv2.imshow("Image", image)
+            udp_client_socket.sendto(message,(server_address,port))
+            cv2.waitKey(1)
+            cv2.waitKey(1)
