d4356802af743d11a9b039d106a3cc17d4189235
Kevin Russel <kzone159@gmail.com>
2026-07-16 21:56:14 -0500

    cleaning up

--- projecthand.py
+++ projecthand.py
@@ -28,45 +28,47 @@ class Hand_Drone:
         return math.hypot(x2-x1,y2-y1)
 
     def worker(self):
-        )   
+           
         while True:
             ## getting frame
-            success, image = cap.read()
-            img = hand.findHands(image,True)
-            left_hand,right_hand = hand.findposition(image)
-            percent = 0
-            if(len(left_hand)!= 0) and len(right_hand) == 0:
-                thumbpoint = (left_hand[4][1],left_hand[4][2])
-                indexpoint = (left_hand[8][1],left_hand[8][2])
-                hypot = self.calculate_hypot(left_hand[4][1],left_hand[4][2],left_hand[8][1],left_hand[8][2])
-                percent = np.interp(hypot,[30,270],[0,100])
-                if(percent > 40 and percent < 60):
-                    print("HOLD")
-                elif(percent<=40):
-                    print("DOWN")
-                else:
-                    print("UP")
-                img = hand.draw_line(img,thumbpoint,indexpoint)
+            success, image = self.cap.read()
+
+            if success:
+                img = hand.findHands(image,True)
+                left_hand,right_hand = hand.findposition(image)
+                percent = 0
+                if(len(left_hand)!= 0) and len(right_hand) == 0:
+                    thumbpoint = (left_hand[4][1],left_hand[4][2])
+                    indexpoint = (left_hand[8][1],left_hand[8][2])
+                    hypot = self.calculate_hypot(left_hand[4][1],left_hand[4][2],left_hand[8][1],left_hand[8][2])
+                    percent = np.interp(hypot,[30,270],[0,100])
+                    if(percent > 40 and percent < 60):
+                        print("HOLD")
+                    elif(percent<=40):
+                        print("DOWN")
+                    else:
+                        print("UP")
+                    img = hand.draw_line(img,thumbpoint,indexpoint)
+                    
+                # elif(len(left_hand)!= 0) and len(right_hand) != 0:
+                #     leftpoint = (left_hand[8][1],left_hand[8][2])
+                #     rightpoint = (right_hand[8][1],right_hand[8][2])
+                #     img = hand.draw_line(img,leftpoint,rightpoint)
+                cTime = time.time()
+                fps = 1/(cTime-pTime)
+                pTime = cTime
+
+
+
+                filling = np.interp(percent,[0,100],[400,150])
+
+
+                cv2.putText(image,f"{str(int(fps))} fps",(10,50),cv2.FONT_HERSHEY_COMPLEX,1,(255,8,255))
+                cv2.putText(image,str(int(percent)),(500,400),cv2.FONT_HERSHEY_COMPLEX,2,(255,8,255))
                 
-            # elif(len(left_hand)!= 0) and len(right_hand) != 0:
-            #     leftpoint = (left_hand[8][1],left_hand[8][2])
-            #     rightpoint = (right_hand[8][1],right_hand[8][2])
-            #     img = hand.draw_line(img,leftpoint,rightpoint)
-            cTime = time.time()
-            fps = 1/(cTime-pTime)
-            pTime = cTime
-
-
-
-            filling = np.interp(percent,[0,100],[400,150])
-
-
-            cv2.putText(image,f"{str(int(fps))} fps",(10,50),cv2.FONT_HERSHEY_COMPLEX,1,(255,8,255))
-            cv2.putText(image,str(int(percent)),(500,400),cv2.FONT_HERSHEY_COMPLEX,2,(255,8,255))
-            
-            cv2.rectangle(img,(50,150),(85,400),(0,255,0),3)
-            cv2.rectangle(img,(50,int(filling)),(85,400),(0,255,0),cv2.FILLED)
-            cv2.imshow("Image", image)
-            udp_client_socket.sendto(message,(server_address,port))
-            cv2.waitKey(1)
-            cv2.waitKey(1)
+                cv2.rectangle(img,(50,150),(85,400),(0,255,0),3)
+                cv2.rectangle(img,(50,int(filling)),(85,400),(0,255,0),cv2.FILLED)
+                cv2.imshow("Image", image)
+                udp_client_socket.sendto(message,(server_address,port))
+                cv2.waitKey(1)
+                cv2.waitKey(1)
